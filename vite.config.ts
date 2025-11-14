f544565db09eeb1f4069f3e46f40d52a397a4298
박율 <bahk-yul@bag-yul-ui-MacBookPro.local>
2025-11-14 13:23:31 +0900

    Fix duplicate base config in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // 이 아랫줄에 base 설정을 추가했습니다.
-      base: "/test_myReactPJ/", 
+      base: '/test_myReactPJ/',
       server: {
         port: 3000,
         host: '0.0.0.0',
